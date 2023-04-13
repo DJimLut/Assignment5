c3c3a9ec477aea9eb8ccda053b35e5cacc8d7c2d
David Jimenez <djimenez@murraystate.edu>
2023-04-13 14:21:52 -0400

    implement main method for proper output

--- src/main/SortComparison.java
+++ src/main/SortComparison.java
@@ -9,6 +9,8 @@
 
 package main;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class SortComparison {
     public static void radixSort(String[] data) {
         // TODO: Implement Method
@@ -18,7 +20,37 @@ public class SortComparison {
         // TODO: Implement Method
     }
 
-    public static void Main(String[] args) {
-        // TODO: Implement Method
+    public static void main(String[] args) {
+        boolean useLetters = true;
+        boolean useNumbers = false;
+
+        String[][] radixArr = new String[200000][25];
+
+        for(int i = 0; i < radixArr.length; i++) {
+            for(int j = 0; j < radixArr[i].length; j++) {
+                // For every string in every string array, assign a random string with its index as its length, using only letters
+                radixArr[i][j] = RandomStringUtils.random(j + 1, useLetters, useNumbers); 
+            }
+        }
+
+        String[][] heapArr = radixArr.clone(); // Clone the array for heapSorting
+
+        System.out.println("ArrLen\tStrLen\tHeap\tRadix\n======\t======\t====\t=====");
+
+        long startTime;
+        long radixTime;
+        long heapTime;
+        for(int i = 0; i < 25; i++) {
+            // Sort every string array, calculate the time and format it
+            startTime = System.nanoTime();
+            radixSort(radixArr[i]);
+            radixTime = (System.nanoTime() - startTime) / 1000000;
+
+            startTime = System.nanoTime();
+            heapSort(heapArr[i]);
+            heapTime = (System.nanoTime() - startTime) / 1000000;
+
+            System.out.printf("%d\t%d\t%d\t%d%n", radixArr.length, radixArr[i][i].length(), heapTime, radixTime);
+        }
     }
 }
