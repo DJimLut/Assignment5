33dfd0d7eb47aa714f1c9ac4c436d8310bab3acf
Mason Richardson <mrichardson3065@gmail.com>
2023-04-16 02:25:07 -0500

    Implement heapSort function
    Implement heap sort, add common-lang3.jar module, fix console output format

--- src/main/SortComparison.java
+++ src/main/SortComparison.java
@@ -12,13 +12,83 @@ package main;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class SortComparison {
+
+    //Radix Sort
     public static void radixSort(String[] data) {
         // TODO: Implement Method
     }
 
-    public static void heapSort(String[] data) {
-        // TODO: Implement Method
+
+    //Heap Sort
+    //Source: https://www.geeksforgeeks.org/lexicographical-ordering-using-heap-sort/
+    static int x = -1;
+    static String[] heap = new String[200000];
+    public static void heapForm(String k) {
+        x++;
+        heap[x] = k;
+        int child = x;
+        String tmp;
+        int index = x/2;
+
+        while (index >= 0) {
+            if(heap[index].compareTo(heap[child]) > 0) {
+                tmp = heap[index];
+                heap[index] = heap[child];
+                heap[child] = tmp;
+                child = index;
+
+                index = index/2;
+            }
+            else {
+                break;
+            }
+        }
     }
+    public static void sort() {
+        int left, right;
+        while(x >= 0) {
+            String k;
+            k = heap[0];
+            heap[0] = heap[x];
+            x = x - 1;
+            String tmp;
+            int index = 0;
+            int length = x;
+
+            left = 1;
+            right = left + 1;
+
+            while(left <= length) {
+                if(heap[index].compareTo(heap[left]) <= 0 && heap[index].compareTo(heap[right]) <= 0) {
+                    break;
+                }
+                else {
+                    if(heap[left].compareTo(heap[right]) < 0) {
+                        tmp = heap[index];
+                        heap[index] = heap[left];
+                        heap[left] = tmp;
+                        index = left;
+                    }
+                    else {
+                        tmp = heap[index];
+                        heap[index] = heap[right];
+                        heap[right] = tmp;
+                        index = right;
+                    }
+                }
+
+                left = 2 * left;
+                right = left + 1;
+            }
+        }
+    }
+    public static void heapSort(String[] data) {
+        for(int i = 0; i < data.length; i++) {
+            heapForm(data[i]);
+        }
+        sort();
+    }
+
 
     public static void main(String[] args) {
         boolean useLetters = true;
@@ -50,7 +120,7 @@ public class SortComparison {
             heapSort(heapArr[i]);
             heapTime = (System.nanoTime() - startTime) / 1000000;
 
-            System.out.printf("%d\t%d\t%d\t%d%n", radixArr[i].length, radixArr[i][i].length(), heapTime, radixTime);
+            System.out.printf("%d\t%d\t\t%d\t\t%d%n", radixArr[i].length, radixArr[i][i].length(), heapTime, radixTime);
         }
     }
 }
