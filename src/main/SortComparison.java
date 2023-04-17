f397076d00243c25e850240906878ebac7bc401e
bfloyd3 <125699485+bfloyd3@users.noreply.github.com>
2023-04-17 02:01:38 -0500

    Implemented radix sort; unknown RandomString error

--- src/main/SortComparison.java
+++ src/main/SortComparison.java
@@ -8,14 +8,66 @@
  */
 
 package main;
-
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class SortComparison {
 
     //Radix Sort
-    public static void radixSort(String[] data) {
-        // TODO: Implement Method
+    //Credit: GeeksforGeeks, https://www.geeksforgeeks.org/radix-sort/
+    public static void radixSort(String input[], 
+                                 int radix,
+                                 int width)
+    {
+        for (int i = 0; i < width; i++) {
+            performRadixSort(input, i, radix);
+        }
+    }
+    public static void performRadixSort(String input[],
+                                        int position,
+                                        int radix)
+    {
+        
+        // Creating a temporary count array
+        int countArray[] = new int[radix];
+        int nos = input.length;
+        
+        // Populating the count array
+        for (String value : input) {
+            countArray[getDigit(position, 
+                                value, radix)]++;
+        }
+      
+        // Normalizing count array
+        for (int i = 1; i < radix; i++) {
+            countArray[i]
+                = countArray[i] 
+                + countArray[i - 1];
+        }
+      
+        String tempArray[] = new String[nos];
+        // Building the final temporary array
+        for (int i = nos - 1; i >= 0; i--) {
+            tempArray[--countArray[getDigit(
+                position, input[i], radix)]]
+                = input[i];
+        }
+        // Copying into the actual array
+        for (int i = 0; i < nos; i++) {
+            input[i] = tempArray[i];
+        }
+    }
+    // Hashing the input value, radix = 26
+    // It takes the character at 
+    // (length - position) location
+    // and convert it to ascii value and 
+    // return the ascii value
+    public static int getDigit(int position, 
+                               String value,
+                               int radix)
+    {
+        int nos = value.length() - 1;
+        char c = value.toLowerCase().charAt(nos - position);
+        return (int)c - 97;
     }
 
 
@@ -113,7 +165,7 @@ public class SortComparison {
         for(int i = 0; i < radixArr.length; i++) {
             // Sort every string array, calculate the time and format it
             startTime = System.nanoTime();
-            radixSort(radixArr[i]);
+            radixSort(radixArr[i], 26, radixArr[0].length);
             radixTime = (System.nanoTime() - startTime) / 1000000;
 
             startTime = System.nanoTime();
